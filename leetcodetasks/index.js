2c78ae1a37cf13fb43ff27b80f3efc3130f425aa
AytacRustemli <aytacrustmli93@gmail.com>
2023-05-05 12:15:36 +0400

    prototype

--- leetcodetasks/index.js
+++ leetcodetasks/index.js
@@ -3,7 +3,7 @@ var strStr = function (haystack, needle) {
   return index >= 0 ? index : -1;
 };
 
-console.log(strStr("sadbutsad", "sad"));
+// console.log(strStr("sadbutsad", "sad"));
 
 
 var mySqrt = function (x) {
@@ -18,20 +18,30 @@ var mySqrt = function (x) {
   return result - 1;
 };
 
-console.log(mySqrt(64));
+// console.log(mySqrt(64));
 
 
 function singleNumber(nums) {
-  let result = 0;
+  let count = {};
+
   for (let i = 0; i < nums.length; i++) {
-    result = result ^ nums[i];
+    if (count[nums[i]] === undefined) {
+      count[nums[i]] = 1;
+    } else {
+      count[nums[i]] += 1;
+    }
   }
-  return result;
+
+  for (let key in count) {
+    if (count[key] === 1) {
+      return parseInt(key);
+    }
+  }
 }
 
 
-console.log(singleNumber([2, 2, 1]));
-console.log(singleNumber([4, 1, 2, 1, 2]));
+// console.log(singleNumber([2, 2, 1]));
+// console.log(singleNumber([4, 1, 2, 1, 2]));
 
 function isHappy(n) {
   let seen = new Set();
@@ -48,4 +58,107 @@ function isHappy(n) {
   return n === 1;
 }
 
-console.log(isHappy(2));
+// console.log(isHappy(2));
+
+
+String.prototype.insertplus = function () {
+  let mid = Math.floor(this.length / 2);
+  return this.slice(0, mid) + " + " + this.slice(mid);
+}
+
+console.log("test".insertplus());
+
+
+
+Number.prototype.double = function () {
+  return this.valueOf() * 2;
+}
+
+console.log((5).double());
+
+
+Array.prototype.push = function (element) {
+  return [...this, element];
+}
+
+const arr = [1, 2, 3, 4, 5]
+console.log(arr.push(6));
+
+
+
+function add(num) {
+  let total = 0;
+  function closure() {
+    total += num;
+    return total;
+  }
+  return closure;
+}
+
+
+const addFive = add(5);
+console.log(addFive());
+console.log(addFive());
+
+// Array.prototype.getArea = function() {
+//   for (let i = 0; i < this.length; i++) {
+//     return this[i] * this[i+1];
+//     // console.log(this[i]);
+//   }
+// }
+
+// const ary = [4,5]
+// console.log(ary.getArea());
+
+
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+}
+
+Rectangle.prototype.getArea = function () {
+  return this.width * this.height;
+}
+
+const rectangle = new Rectangle(4,5);
+console.log(rectangle.getArea());
+
+
+
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+}
+
+Person.prototype.introduce = function () {
+  return "Name : " + this.name + " Age : " + this.age
+}
+
+const person = new Person("Aytac",20);
+console.log(person.introduce());
+
+
+
+function Animal(name) {
+  this.name = name;
+}
+
+Animal.prototype.eat = function () {
+  return this.name + " yemek yeyir ";
+}
+
+const animal = new Animal("Luna");
+console.log(animal.eat());
+
+
+
+Array.prototype.last = function () {
+  if (this.length == 0) {
+    return -1;
+  }
+  return this[this.length -1];
+}
+
+const a = [1,2,3,4]
+console.log(a.last());
+
